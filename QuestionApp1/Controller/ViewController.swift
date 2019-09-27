ccf9e15316295390e6685b7e69bf7c8072b4afe5
rui-watanabe <wataru610806@gmail.com>
2019-09-27 10:48:44 +0900

    initial commit

---
+++ QuestionApp1/Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  QuestionApp1
+//
+//  Created by watar on 2019/09/26.
+//  Copyright © 2019 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var maxScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+    
+    
+    @IBAction func answer(_ sender: Any) {
+        
+        if (sender as AnyObject).tag == 1{
+            
+            
+            
+        }else if (sender as AnyObject).tag == 2{
+        }
+        
+    }
+    
+
+}
+
